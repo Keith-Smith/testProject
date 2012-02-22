3f90dd7ff38d29c4a50443205cf86fd1131ff4b8
Keith Smith <Keith.E.Smith90@gmail.com>
2012-02-21 19:52:40 -0600

    second commit

---
+++ hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Keith-Smith
+ */
+public class hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // simple output
+        System.out.printf("Hello, here is an example file");
+    }
+}
